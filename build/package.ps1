47a7d69b1cf6405eb6fa8fd85ca7ed2dc9289592
Richard <niemand.richard@gmail.com>
2018-12-11 15:15:22 -0700

    Attempting to execute tests from the PS script

--- build/package.ps1
+++ build/package.ps1
@@ -5,27 +5,24 @@
 
 $buildDir = $env:APPVEYOR_BUILD_FOLDER
 $fullBuildNumber = $env:APPVEYOR_BUILD_VERSION
-$rnCommonDir = $buildDir + "\src\Rn.Common";
-$nugetFile = $rnCommonDir + "\Rn.Common." + $fullBuildNumber + ".nupkg";
+$projectDir = $buildDir + "\src\Rn.Common";
+$testDir = $buildDir + "\src\Rn.CommonTests";
+$nugetFile = $projectDir + "\Rn.Common." + $fullBuildNumber + ".nupkg";
 
 Write-Host "Checking .NET Core version" -ForegroundColor Green
 & dotnet --version
 
-Write-Host "Testing build number variables" -ForegroundColor Green
-Write-Host $env:APPVEYOR_BUILD_NUMBER
-Write-Host $env:APPVEYOR_BUILD_VERSION
+Write-Host "Running tests" -ForegroundColor Green
+cd $testDir
+& dotnet restore Rn.CommonTests.csproj --verbosity m
+& dotnet test
 
 Write-Host "Building NuGet package" -ForegroundColor Green
-cd $rnCommonDir
-& dotnet pack -c Release /p:PackageVersion=$fullBuildNumber -o $rnCommonDir
+cd $projectDir
+& dotnet pack -c Release /p:PackageVersion=$fullBuildNumber -o $projectDir
+
+Write-Host "Saving artifacts..." -ForegroundColor Green
+Get-ChildItem .\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
 Write-Host "Attempting to publish NuGet package" -ForegroundColor Green
 & nuget push $nugetFile -ApiKey $env:NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
-
-
-# Successfully created package 'C:\projects\rn-common\src\Rn.Common\bin\Release\Rn.Common.1.0.19.nupkg'
-
-
-#  #- cmd: dotnet nuget push Rn.Common.1.0.%version%.nupkg  -k %NuGetKey%  -s https://www.nuget.org/
-
-#write-host $buildFolder
