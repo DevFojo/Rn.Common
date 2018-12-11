7e7c94f8dce522c8b9d41d2384e5f1dc0cf534dd
Richard <niemand.richard@gmail.com>
2018-12-11 15:28:41 -0700

    Testing build script

--- build/package.ps1
+++ build/package.ps1
@@ -1,5 +1,4 @@
 # https://www.appveyor.com/docs/environment-variables/
-
 # APPVEYOR_BUILD_NUMBER - build number        | 17
 # APPVEYOR_BUILD_VERSION - build version      | 1.0.17
 
@@ -12,17 +11,31 @@ $nugetFile = $projectDir + "\Rn.Common." + $fullBuildNumber + ".nupkg";
 Write-Host "Checking .NET Core version" -ForegroundColor Green
 & dotnet --version
 
+# Discover and run tests
 Write-Host "Running tests" -ForegroundColor Green
 cd $testDir
 & dotnet restore Rn.CommonTests.csproj --verbosity m
-& dotnet test
+$testOutput = & dotnet test | Out-String
+Write-Host $testOutput
 
+# Ensure that the tests passed
+if ($testOutput.Contains("Test Run Successful.") -eq $False) {
+  Write-Host "Build failed!";
+  Exit;
+}
+
+# Generate a NuGet package for publishing
 Write-Host "Building NuGet package" -ForegroundColor Green
 cd $projectDir
 & dotnet pack -c Release /p:PackageVersion=$fullBuildNumber -o $projectDir
 
+# Save generated artifacts
 Write-Host "Saving artifacts..." -ForegroundColor Green
 Get-ChildItem .\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
+# Publish package to NuGet
 Write-Host "Attempting to publish NuGet package" -ForegroundColor Green
 & nuget push $nugetFile -ApiKey $env:NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
+
+# Done
+Write-Host "All done." -ForegroundColor Green
