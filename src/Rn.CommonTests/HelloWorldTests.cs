d7c18d04e354f7064828e230280b48b258f71f2e
Richard <niemand.richard@gmail.com>
2018-12-10 21:46:50 -0700

    Added test project

---
+++ src/Rn.CommonTests/HelloWorldTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Rn.CommonTests
+{
+  [TestFixture]
+  public class HelloWorldTests
+  {
+    [Test]
+    public void HelloWorld_GivenCalled_ShouldSayHelloWorld()
+    {
+      Assert.AreEqual("Hello World", HelloWorld());
+    }
+
+    // Hello World
+    private static string HelloWorld()
+    {
+      return "Hello World";
+    }
+  }
+}
