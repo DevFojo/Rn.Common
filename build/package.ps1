27a0632b65a9fa5f940b220447ca26d591893195
Richard <niemand.richard@gmail.com>
2018-12-11 14:55:23 -0700

    testing .net core access

--- build/package.ps1
+++ build/package.ps1
@@ -1,3 +1,21 @@
-$buildFolder = $env:APPVEYOR_BUILD_FOLDER
+# https://www.appveyor.com/docs/environment-variables/
 
-write-host $buildFolder
+# APPVEYOR_BUILD_NUMBER - build number
+# APPVEYOR_BUILD_VERSION - build version
+
+#$buildDir = $env:APPVEYOR_BUILD_FOLDER
+#$rnCommonDir = $buildDir + "\src\Rn.Common";
+
+Write-Host "Checking .NET Core version" -ForegroundColor Green
+
+& dotnet --version
+
+Write-Host "Testing build number variables" -ForegroundColor Green
+Write-Host $env:APPVEYOR_BUILD_NUMBER
+Write-Host $env:APPVEYOR_BUILD_VERSION
+
+#  - cmd: dotnet pack -c Release
+#  #- cmd: cd bin/Release
+#  #- cmd: dotnet nuget push Rn.Common.1.0.%version%.nupkg  -k %NuGetKey%  -s https://www.nuget.org/
+
+#write-host $buildFolder
