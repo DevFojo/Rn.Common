5bd41a7ab79c9d9dd84669a8dbb4c6f3515adabf
Richard <niemand.richard@gmail.com>
2018-12-11 14:59:31 -0700

    Trying to build with correct build number

--- build/package.ps1
+++ build/package.ps1
@@ -1,18 +1,21 @@
 # https://www.appveyor.com/docs/environment-variables/
 
-# APPVEYOR_BUILD_NUMBER - build number
-# APPVEYOR_BUILD_VERSION - build version
+# APPVEYOR_BUILD_NUMBER - build number        | 17
+# APPVEYOR_BUILD_VERSION - build version      | 1.0.17
 
-#$buildDir = $env:APPVEYOR_BUILD_FOLDER
+$buildDir = $env:APPVEYOR_BUILD_FOLDER
+$fullBuildNumber = $env:APPVEYOR_BUILD_VERSION
 #$rnCommonDir = $buildDir + "\src\Rn.Common";
 
 Write-Host "Checking .NET Core version" -ForegroundColor Green
-
 & dotnet --version
 
 Write-Host "Testing build number variables" -ForegroundColor Green
 Write-Host $env:APPVEYOR_BUILD_NUMBER
 Write-Host $env:APPVEYOR_BUILD_VERSION
+
+Write-Host "Building NuGet package" -ForegroundColor Green
+& dotnet pack -c Release /p:PackageVersion=$fullBuildNumber
 
 #  - cmd: dotnet pack -c Release
 #  #- cmd: cd bin/Release
