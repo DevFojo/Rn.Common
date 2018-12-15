caf1017dd1397ebca09faba7e55de3ad6f2f1d9b
Richard <richard.niemand@derivco.co.za>
2018-12-14 22:36:16 -0700

    Added start of IHttpClient

---
+++ src/Rn.Common/Net/IHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Rn.Common.Net
+{
+  public interface IHttpClient
+  {
+    Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
+    Task<string> GetPageTextAsync(string requestUri);
+  }
+}
---
+++ src/Rn.Common/Net/RnHttpClient.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Rn.Common.Net
+{
+  public class RnHttpClient : IHttpClient
+  {
+    private readonly ILogger<RnHttpClient> _logger;
+    private readonly HttpClient _httpClient;
+
+    public RnHttpClient(ILogger<RnHttpClient> logger)
+    {
+      _logger = logger;
+      _httpClient = new HttpClient();
+    }
+
+    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+    {
+      return await _httpClient.SendAsync(request);
+    }
+
+    public async Task<string> GetPageTextAsync(string requestUri)
+    {
+      var stopwatch = Stopwatch.StartNew();
+      var result = await _httpClient.GetAsync(requestUri);
+      result.EnsureSuccessStatusCode();
+
+      var responseStr = await result.Content.ReadAsStringAsync();
+      stopwatch.Stop();
+      _logger.LogDebug($"Fetched '{requestUri}' in {stopwatch.ElapsedMilliseconds} ms");
+
+      return responseStr;
+    }
+  }
+}
